6df22fdd8e74a2d665bca645334e4beeae688ed7
EricSoklowski <ericsoko2002@gmail.com>
2022-10-03 09:48:14 -0400

    add schemas

---
+++ models/character.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const commentSchema = new Schema ({
+  reviewer: String,
+  avatar: String,
+  content: String,
+}, {
+  timestamps: true,
+});
+
+const characterSchema = new Schema({
+  name: String,
+  race: {
+    type: String,
+    enum: ['Dragonborn', 'Dwarf', 'Elf', 'Gnome', 'Half-Elf', 'Halfling', 'Half-Orc', 'Human', 'Tiefling']
+  },
+  class: {
+    type: String,
+    enum: ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rouge', 'Sorcerer', 'Warlock', 'Wizard']
+  },
+  level: {
+    type: Number,
+    min: 1,
+    max: 20,
+  },
+  background: {
+    type: String,
+    enum: ['Acolyte', 'Criminal/Spy', 'Folk Hero', 'Noble', 'Sage', 'Soldier'
+    ]
+  },
+  comments: [commentSchema],
+})
